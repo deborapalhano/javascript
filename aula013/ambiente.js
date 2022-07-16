f7dc76cff192b8f2163a9594d6c89d1f486aa717
Debora Palhano <103783091+DeboraPalhano@users.noreply.github.com>
2022-07-16 21:26:44 +0100

    criei novos docs

---
+++ aula013/ambiente.js
@@ -0,0 +1,19 @@
+//Maneira 1
+/*d = 1
+while(d <=10){
+    console.log(d)
+    d++
+} */
+
+
+//Maneira 2 (do..while)
+/*d = 1
+do{
+    console.log(d)
+    d++
+} while (d <=10) */
+
+//Maneira 3
+for( d=1 ; d <= 10 ; d++ ){
+    console.log(d)
+}
